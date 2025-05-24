ff4ee721e1bcdc2017aa95f6e2e7be760e19b4e8
rizkirenden <rizkirenden@gmail.com>
2025-05-25 00:13:22 +0800

    model tokos

--- back-end/model/TokoModels.js
+++ back-end/model/TokoModels.js
@@ -1,6 +1,12 @@
 const pool = require("../config/database");
 
 const TokoModel = {
+  async getAllToko() {
+    const query = "SELECT * FROM tokos";
+    const [rows] = await pool.query(query);
+    return rows;
+  },
+
   async insertToko(toko) {
     const { nama_toko, nama_pemilik, no_telp, alamat_toko, alamat_pemilik } =
       toko;
@@ -16,6 +22,34 @@ const TokoModel = {
     ];
     const [result] = await pool.query(query, values);
     return result.insertId;
+  },
+
+  async updateToko(toko_id, toko) {
+    const { nama_toko, nama_pemilik, no_telp, alamat_toko, alamat_pemilik } =
+      toko;
+    const query = `UPDATE tokos SET nama_toko = ?, nama_pemilik = ?, no_telp = ?, alamat_toko = ?, alamat_pemilik = ? WHERE toko_id = ?`;
+    const value = [
+      nama_toko,
+      nama_pemilik,
+      no_telp,
+      alamat_toko,
+      alamat_pemilik,
+      toko_id,
+    ];
+    const [result] = await pool.query(query, value);
+    return result.affectedRows;
+  },
+
+  async deleteToko(toko_id) {
+    const query = `DELETE FROM tokos WHERE toko_id = ?`;
+    const [result] = await pool.query(query, [toko_id]);
+    return result.affectedRows;
+  },
+
+  async editToko(toko_id) {
+    const query = `SELECT * FROM tokos WHERE toko_id = ?`;
+    const [rows] = await pool.query(query, [toko_id]);
+    return rows[0];
   },
 };
 module.exports = TokoModel;
--- back-end/service/tokoService.js
+++ back-end/service/tokoService.js
@@ -1,7 +1,23 @@
 const TokoModel = require("../model/TokoModels");
 
+async function getAllToko() {
+  return await TokoModel.getAllToko();
+}
+
 async function createToko(toko) {
   return await TokoModel.insertToko(toko);
 }
 
-module.exports = { createToko };
+async function updateToko(toko_id, toko) {
+  return await TokoModel.updateToko(toko_id, toko);
+}
+
+async function deleteToko(toko_id) {
+  return await TokoModel.deleteToko(toko_id);
+}
+
+async function editToko(toko_id) {
+  return await TokoModel.editToko(toko_id);
+}
+
+module.exports = { getAllToko, createToko, updateToko, deleteToko, editToko };
