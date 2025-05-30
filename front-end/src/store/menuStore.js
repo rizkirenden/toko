86f29b8f5dc1e7473cd447aacb68e0a21976eacf
rizkirenden <rizkirenden@gmail.com>
2025-05-31 00:04:26 +0800

    global state menu

---
+++ front-end/src/store/menuStore.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { create } from "zustand";
+
+const useMenuStore = create((set) => ({
+  menu: [],
+  loading: false,
+  error: null,
+
+  fetchMenu: async () => {
+    set({ loading: true, error: null });
+    try {
+      const response = await axios.get("http://localhost:3000/api/products");
+
+      set({ menus: response.data.data, loading: false });
+    } catch (err) {
+      set({
+        error: err.response?.data?.error || err.message,
+        loading: false,
+      });
+    }
+  },
+}));
+export default useMenuStore;
