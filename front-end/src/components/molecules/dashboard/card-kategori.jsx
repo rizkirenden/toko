0b16c53d2bf69c4d9f05147f673a32d086a187f8
rizkirenden <rizkirenden@gmail.com>
2025-06-11 23:29:43 +0800

    dashboard card

---
+++ front-end/src/components/molecules/dashboard/card-kategori.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Cardkategori = () => {
+  return <div>card.kategori</div>;
+};
---
+++ front-end/src/components/molecules/dashboard/card-produk.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Cardproduk = () => {
+  return <div>card-produk</div>;
+};
