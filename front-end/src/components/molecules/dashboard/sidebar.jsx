34512a514a875062d42fa7493b5b774d1822e97c
rizkirenden <rizkirenden@gmail.com>
2025-06-12 00:35:14 +0800

    sidebar

---
+++ front-end/src/components/molecules/dashboard/sidebar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Card } from "../../atoms/card";
+import { Navitem } from "../../atoms/navitem";
+import { Icon } from "../../atoms/icon";
+
+export const Sidebar = () => {
+  return (
+    <div>
+      <Card>
+        <nav>
+          <Navitem
+            label="dahsboard"
+            href=""
+            text=""
+            icon={<Icon name="" className="" />}
+          />
+          <Navitem
+            label="toko"
+            href=""
+            text=""
+            icon={<Icon name="" className="" />}
+          />
+          <Navitem
+            label="produk"
+            href=""
+            text=""
+            icon={<Icon name="" className="" />}
+          />
+          <Navitem
+            label="category"
+            href=""
+            text=""
+            icon={<Icon name="" className="" />}
+          />
+          <Navitem
+            label="register"
+            href=""
+            text=""
+            icon={<Icon name="" className="" />}
+          />
+          <Navitem
+            label="logout"
+            href=""
+            text=""
+            icon={<Icon name="" className="" />}
+          />
+        </nav>
+      </Card>
+    </div>
+  );
+};
