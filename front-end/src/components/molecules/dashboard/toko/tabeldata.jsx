6f4602d6d84f7ba16667b18915b816ddcee235a4
rizkirenden <rizkirenden@gmail.com>
2025-06-19 21:17:05 +0800

    tabeldata

--- front-end/src/components/molecules/dashboard/toko/tabeldata.jsx
+++ front-end/src/components/molecules/dashboard/toko/tabeldata.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import { Card } from "../../../atoms/card";
 
-export const Tabeldata = () => {
+export const Tabeldata = ({ data }) => {
   return (
-    <div>
-      <Card></Card>
-    </div>
+    <Card className="p-4">
+      <table className="w-full text-left border-collapse">
+        <thead>
+          <tr>
+            <th className="border-b p-2">ID</th>
+            <th className="border-b p-2">Nama Toko</th>
+            <th className="border-b p-2">Nama Pemilik</th>
+            <th className="border-b p-2">No Telp</th>
+            <th className="border-b p-2">Alamat Toko</th>
+            <th className="border-b p-2">Alamat Pemilik</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((item) => (
+            <tr key={item.id} className="hover:bg-gray-100">
+              <td className="border-b p-2">{item.id_toko}</td>
+              <td className="border-b p-2">{item.nama_toko}</td>
+              <td className="border-b p-2">{item.nama_pemilik}</td>
+              <td className="border-b p-2">{item.no_telp}</td>
+              <td className="border-b p-2">{item.alamat_toko}</td>
+              <td className="border-b p-2">{item.alamat_pemilik}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Card>
   );
 };
