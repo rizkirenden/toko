eb63c3223a6c82d26dc093850dc3fafdd9345506
rizkirenden <rizkirenden@gmail.com>
2025-06-27 10:24:10 +0800

    useuser

--- front-end/src/store/userStore.js
+++ front-end/src/store/userStore.js
@@ -2,14 +2,18 @@ import axios from "axios";
 import { create } from "zustand";
 const useUserStore = create((set) => ({
   users: [],
+  total: 1,
   loading: false,
   error: null,
 
-  fetchUsers: async () => {
+  fetchUsers: async ({ page = 1, limit = 5, search = "" } = {}) => {
     set({ loading: true, error: null });
     try {
-      const response = await axios.get("http://localhost:3000/api/users");
+      const params = new URLSearchParams({ page, limit, search });
 
+      const response = await axios.get(
+        `http://localhost:3000/api/users?${params.toString()}`
+      );
       set({ users: response.data.data, loading: false });
     } catch (err) {
       set({
