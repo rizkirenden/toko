20834661d735ecab9a568db8f0ee275e1103f371
rizkirenden <rizkirenden@gmail.com>
2025-06-30 11:51:47 +0800

    kategori mole

--- front-end/src/components/organisms/dashboard/kategori/datatabel.jsx
+++ front-end/src/components/organisms/dashboard/kategori/datatabel.jsx
@@ -1,17 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Tabeldata } from "../../../molecules/dashboard/kategori/tabeldata";
 import useCategoryStore from "../../../../store/categoryStore";
+import { Formedit } from "../../../molecules/dashboard/kategori/formedit";
 
 const Datatabel = ({ page, limit }) => {
-  const { categoriesData, fetchCategoriesData } = useCategoryStore();
+  const { categoriesData, fetchCategoriesData, deleteCategory } =
+    useCategoryStore();
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
   useEffect(() => {
     fetchCategoriesData({ page, limit });
   }, [page, limit, fetchCategoriesData]);
 
+  const handleEdit = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleDelete = async (id) => {
+    const konfirmasi = confirm("Yakin ingin menghapus kategori ini?");
+    if (!konfirmasi) return;
+    try {
+      await deleteCategory(id);
+      fetchCategoriesData({ page, limit });
+      alert("Kategori berhasil dihapus");
+    } catch (err) {
+      alert("Gagal menghapus kategori:" + err);
+    }
+  };
   return (
     <div className="space-y-4">
-      <Tabeldata data={categoriesData} />
+      <Tabeldata
+        data={categoriesData}
+        onEdit={handleEdit}
+        onDelete={handleDelete}
+      />
     </div>
   );
 };
