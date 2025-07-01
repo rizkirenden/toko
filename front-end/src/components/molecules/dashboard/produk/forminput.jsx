12f129b70b02cf96d2075bb14eef158678769809
rizkirenden <rizkirenden@gmail.com>
2025-07-01 14:01:59 +0800

    produk

--- front-end/src/components/molecules/dashboard/produk/forminput.jsx
+++ front-end/src/components/molecules/dashboard/produk/forminput.jsx
@@ -1,5 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { Input } from "../../../atoms/input";
+import useProdukStore from "../../../../store/produkStore";
 
 export const Forminput = () => {
+  const { addProduct } = useProdukStore();
+
+  const [form, setForm] = useState({
+    name: "",
+    harga: "",
+    deskripsi_bahan: "",
+    gambar_product: "",
+    video_product: "",
+    status: "",
+    nama_toko: "",
+    no_telp: "",
+    nama_kategori: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const FileChange = (e) => {
+    const { name, files } = e.target;
+    setForm((prev) => ({ ...prev, [name]: files[0] }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    for (const key in form) {
+      formData.append(key, form[key]);
+    }
+
+    try {
+      await addProduct(formData);
+      alert("Produk berhasil ditambah");
+
+      setForm({
+        name: "",
+        harga: "",
+        deskripsi_bahan: "",
+        gambar_product: "",
+        video_product: "",
+        status: "",
+        nama_toko: "",
+        no_telp: "",
+        nama_kategori: "",
+      });
+    } catch (err) {
+      alert("Gagal menambah Produk: " + err);
+    }
+  };
+
   return <div>forminput</div>;
 };
