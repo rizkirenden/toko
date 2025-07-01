ce35c7eaf5bf1a4eb0724a0739e622d925eaf472
rizkirenden <rizkirenden@gmail.com>
2025-07-01 17:46:47 +0800

    kategori

--- front-end/src/components/molecules/dashboard/produk/forminput.jsx
+++ front-end/src/components/molecules/dashboard/produk/forminput.jsx
@@ -55,5 +55,54 @@ export const Forminput = () => {
     }
   };
 
-  return <div>forminput</div>;
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="space-y-4 bg-white p-6 rounded shadow max-w-lg"
+    >
+      <Input
+        name="name"
+        placeholder="Nama Produk"
+        value={form.name}
+        onChange={handleChange}
+        required
+        className="w-full"
+      />
+      <Input
+        name="harga"
+        placeholder="Harga"
+        value={form.harga}
+        onChange={handleChange}
+        required
+        className="w-full"
+      />
+      <Input
+        name="deskripsi_bahan"
+        placeholder="Deskripsi Bahan"
+        value={form.deskripsi_bahan}
+        required
+        className="w-full"
+      />
+      <Input
+        type="file"
+        name="gambar_product"
+        accept="image/*"
+        onChange={FileChange}
+        required
+      />
+      <Input
+        type="file"
+        name="video_product"
+        accept="video/*"
+        onChange={FileChange}
+        required
+      />
+      <Input
+        name="nama_kategori"
+        placeholder="Nama Kategori"
+        value={form.nama_kategori}
+        onChange={handleChange}
+      />
+    </form>
+  );
 };
