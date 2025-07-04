bd96a0e3b6200db948da389b566c129686d1f992
rizkirenden <rizkirenden@gmail.com>
2025-07-04 23:56:43 +0800

    login

--- front-end/src/components/organisms/login/login.jsx
+++ front-end/src/components/organisms/login/login.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Content } from "../../molecules/login/content";
 import { Loginbtn } from "../../molecules/login/login-btn";
 import { Logininput } from "../../molecules/login/login-input";
+import Authstore from "../../../store/authStore";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [form, setForm] = useState({ email: "", password: "" });
+  const { setAuth } = Authstore();
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
   return (
     <div className="h-full flex items-center justify-center ">
       <div className="w-full max-w-md p-8 ">
